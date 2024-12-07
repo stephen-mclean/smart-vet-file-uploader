954c8b18faa3e7db7bdad0efe26fd77bbdd135d1
Stephen McLean <stephenmclean@MacBook-Air.local>
2024-12-07 17:06:58 +0100

    add initial script

--- cypress/e2e/upload.cy.js
+++ cypress/e2e/upload.cy.js
@@ -1,5 +1,39 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("template spec", () => {
+  it("passes", () => {
+    const username = Cypress.env("USERNAME");
+    const password = Cypress.env("PASSWORD");
+
+    const clientName = Cypress.env("CLIENT_NAME");
+    const petName = Cypress.env("PET_NAME");
+    const visitDate = Cypress.env("VISIT_DATE");
+
+    if (!username || !password || !clientName || !petName || !visitDate) {
+      return;
+    }
+
+    cy.visit("https://clinic.smart-vet.com/Account/LogOn");
+
+    cy.get("#UserName").type(username);
+    cy.get("#Password").type(password);
+    cy.get('button[type="submit"]').click();
+
+    cy.get("a[href='/Customer']").eq(1).click();
+    cy.get("input[class='form-control']").type(clientName);
+    cy.get("button[type='submit']").click();
+
+    cy.get("table").within(() => {
+      cy.get("a").first().click();
+    });
+
+    cy.get("a[href='#patients']").click();
+    cy.contains(petName).click();
+
+    cy.get("a[href='#visits']").click();
+
+    cy.contains(visitDate)
+      .parent("tr")
+      .within(() => {
+        cy.get("a").eq(1).click();
+      });
+  });
+});
